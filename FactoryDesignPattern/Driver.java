c3fc57b8fb188f7305324379351c258284114542
ardo1488 <lalo.gaxiola15@icloud.com>
2025-10-06 20:15:45 -0700

    Changed to rule switch in driver file and closed scanner

--- FactoryDesignPattern/Driver.java
+++ FactoryDesignPattern/Driver.java
@@ -14,27 +14,21 @@ public class Driver {
             System.out.println("Select a diet plan (1- No Restrictions, 2- Paleo, 3- Vegan, 4- Nut Allergy): ");
             int choice = scanner.nextInt();
             switch (choice) {
-                case 1:
-                    mealSingleton.setDietPlan(new NoRestrictionsDietPlan());
-                    break;
-                case 2:
-                    mealSingleton.setDietPlan(new PaleoDietPlan());
-                    break;
-                case 3:
-                    mealSingleton.setDietPlan(new VeganDietPlan());
-                    break;
-                case 4:
-                    mealSingleton.setDietPlan(new NutAllergyDietPlan());
-                    break;
-                default:
+                case 1 -> mealSingleton.setDietPlan(new NoRestrictionsDietPlan());
+                case 2 -> mealSingleton.setDietPlan(new PaleoDietPlan());
+                case 3 -> mealSingleton.setDietPlan(new VeganDietPlan());
+                case 4 -> mealSingleton.setDietPlan(new NutAllergyDietPlan());
+                default -> {
                     System.out.println("Invalid choice. Please select a valid diet plan.");
                     continue;
+                }
             }
             mealSingleton.createMeal();
             System.out.println("Customer " + (i + 1) + " Meal Plan:");
             System.out.println("Carbs: " + mealSingleton.getCarbs());
             System.out.println("Fats: " + mealSingleton.getFats());
             System.out.println("Protein: " + mealSingleton.getProtein() + "\n");
+            scanner.close();
         }
     }
 }
